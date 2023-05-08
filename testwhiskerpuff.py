c098330cb26e3011c1b08959dac037e45239c41e
bustenchops <30851972+bustenchops@users.noreply.github.com>
2023-05-08 10:04:27 -0400

    Update testwhiskerpuff.py
    updated 8may23

--- testwhiskerpuff.py
+++ testwhiskerpuff.py
@@ -15,22 +15,20 @@ rightpuff = 7
 GPIO.setup(leftpuff, GPIO.OUT, initial=0)
 GPIO.setup(rightpuff, GPIO.OUT, initial=0)
 
-# ENTER VARIABLES RANGE TO TEST
-timeoninsec = 1
-inbetweensec = 1
+#Script
+looped = 1
 
-# SCRIPT
-GPIO.output(leftpuff, 1)
-print('left on GPIO 8')
-time.sleep(timeoninsec)
-GPIO.output(leftpuff, 0)
-print('OFF left GPIO 8')
-time.sleep(inbetweensec)
-GPIO.output(rightpuff, 1)
-print('left on GPIO 8')
-time.sleep(timeoninsec)
-GPIO.output(rightpuff, 0)
-print('OFF left GPIO 8')
-time.sleep(inbetweensec)
-
+while looped > 0:
+	GPIO.output(leftpuff,1)
+	print("left on pin 8")
+	time.sleep(5)
+	GPIO.output(leftpuff,0)
+	print("left off pin 8")
+	time.sleep(5)
+	print("right on pin 7")
+	GPIO.output(rightpuff,1)
+	time.sleep(5)
+	GPIO.output(rightpuff,0)
+	print("right off pin 7")
+	time.sleep(5)
 
